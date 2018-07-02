36d324c4518cec4c9a30b0458ed90e07bcff528e
Christopher Etges <c.etges86@gmail.com>
2018-07-02 00:15:05 -0600

    add bulma/login page

---
+++ client/src/components/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Nav = () => (
+    // eslint-disable-next-line
+    <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+            <a className="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+            </a>
+        </div>
+        <div className="navbar-menu">
+            <div className="navbar-start">
+                <a className="navbar-item">
+                    Sign Up
+                </a>
+            </div>
+            <div className="navbar-end">
+                <a className="navbar-item">
+                    Login
+                    </a>
+            </div>
+        </div>
+        {/* <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true">My Profile</span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a> */}
+
+    </nav>
+
+)
+
+export default Nav;
---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NoMatch = () => (
+  <div>
+      <p> 404 page not found </p>
+      </div>
+);
+
+export default NoMatch;

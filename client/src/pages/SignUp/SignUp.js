9b3cbc9acadc2b82fed20133e05963dd66ac6471
Christopher Etges <c.etges86@gmail.com>
2018-07-08 21:56:26 -0600

    work on signup form

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -5,7 +5,21 @@ import "./SignUp.css";
 
 class SignUp extends Component {
 
+    state = {
+        fileLabel: "Choose a profile photo...",
+        imgSrc:"https://bulma.io/images/placeholders/128x128.png"
+    }
 
+    getFileName = () => {
+        var file = document.getElementById("file");
+        console.log(file.files);
+        file.onchange = function () {
+            if (file.files.length > 0) {
+                document.getElementById('filename').innerHTML = file.files[0].name;
+            }
+        };
+        this.setState({ fileLabel: "Select a different photo" })
+    }
 
     render() {
         return (
@@ -18,6 +32,30 @@ class SignUp extends Component {
                     <div className="column is-half is-offset-one-quarter">
                         <div className="card">
                             <div className="card-content">
+                                <div className="media">
+                                    <div className="media-left">
+                                        <figure className="image is-128x128">
+                                            <img alt="profile" src={this.state.imgSrc} />
+                                        </figure>
+                                    </div>
+                                    <div className="media-content">
+                                        <div className="file has-name">
+                                            <label className="file-label">
+                                                <input onInput={this.getFileName} id="file" className="file-input" type="file" name="resume" />
+                                                <span className="file-cta">
+                                                    <span className="file-icon">
+                                                        <i className="fas fa-upload"></i>
+                                                    </span>
+                                                    <span className="file-label">
+                                                        {this.state.fileLabel}
+                                                    </span>
+                                                </span>
+                                                <span id="filename" className="file-name">Upload</span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br />
                                 <div className="field">
                                     <label className="label">Name</label>
                                     <div className="control has-icons-left">
@@ -27,6 +65,7 @@ class SignUp extends Component {
                                         </span>
                                     </div>
                                 </div>
+
                                 <div className="field">
                                     <label className="label">E-mail</label>
                                     <div className="control has-icons-left">
@@ -67,9 +106,9 @@ class SignUp extends Component {
                                         </div>
                                     </div>
                                     <div className="column is-half">
-                                        <label className="label">Current Job</label>
+                                        <label className="label">Desired Job</label>
                                         <div className="control has-icons-left">
-                                            <input className="input is-primary" type="text" placeholder="Company or Job Title" />
+                                            <input className="input is-primary" type="text" placeholder="Job Title" />
                                             <span className="icon is-small is-left">
                                                 <i className="fas fa-briefcase"></i>
                                             </span>

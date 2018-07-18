4d36c28d414e1e0aaf59dba166ee1e900c154e41
tshelton90 <32471437+tshelton90@users.noreply.github.com>
2018-07-17 21:25:37 -0600

    working yearbook page

--- client/src/pages/Yearbook/Yearbook.js
+++ client/src/pages/Yearbook/Yearbook.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
+import YearbookCard from './YearbookCard.js'
 // import logo from "../../img/logo.png";
 //import { Route, Redirect } from "react-router-dom"
 import API from "../../util/API.js";
 import "./Yearbook.css";
-import "./YearbookCard.js"
+
 
 
 class Yearbook extends Component {
 
+    state = {
+        users: []
+    }
+
     componentDidMount() {
         API.getAllUsers().then(res => {
-            console.log(res) 
+            console.log(res)
+            this.setState({users: res.data}) 
         })
         .catch(err => console.log(err))
     }
     render() {
         return(
-            {res.state (user => {
-        return <ImgCard
-            name={this.state.name}
-            email={this.state.email}
-            lang={this.state.lang}
-            job={this.state.job}
-            photo={this.state.photo}
-            {...user} />
-    })}
-)
-    };
+            <div>
+            {this.state.users.map(user => {
+        return <YearbookCard
+                key = {user._id}
+            // name={this.state.name}
+            // email={this.state.email}
+            // lang={this.state.lang}
+            // job={this.state.job}
+            // photo={this.state.photo}
+            {...user} /> })}
+            </div>
+    )}
+
+    
  
 
    

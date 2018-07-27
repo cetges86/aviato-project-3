b782b86f6197086df02530edab1bb3d7778da2fe
Christopher Etges <c.etges86@gmail.com>
2018-07-26 19:50:09 -0600

    update links

--- client/src/pages/Yearbook/YearbookCard.js
+++ client/src/pages/Yearbook/YearbookCard.js
@@ -3,38 +3,39 @@ import React, { Component } from "react";
 class YearbookCard extends Component {
   render() {
     return (
-      
-        <div className= "column is-4">
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by5">
-                <img className="profilePhoto" src={this.props.photo} alt="user profile" />
-              </figure>
-            </div>
-            <div className="card-content">
-              <div className="media">
-                <div className="media-left" />
-                <div className="media-content">
-                  <p className="title is-4">{this.props.name}</p>
-                  <p className="subtitle is-6"><a href={"mailto:" +this.props.email}>Email</a></p>
-                  <p className="subtitle is-6"><a href={this.props.linked}>LinkedIn</a></p>
-                  <p className="subtitle is-6"><a href={this.props.github}>GitHub</a></p>
-                  <p className="subtitle is-6"><a href={this.props.portfolio}>Portfolio</a></p>
-                  <p className="subtitle is-6">{this.props.lang}</p>
-                  <p className="subtitle is-6">{
-                            (this.props.looking)
-                                ? `Desired Job: ${this.props.job}`
-                                : `Current Job: ${this.props.job}`
-                        } </p>
-                  <p className="subtitle is-6">{
-                            (this.props.looking)
-                                ? "Currently Seeking Employment: Yes" : "Not Currently Seeking Employment"
-                        } </p>
-                </div>
+
+      <div className="column is-4">
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by5">
+              <img className="profilePhoto" src={this.props.photo} alt="user profile" />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left" />
+              <div className="media-content">
+                <p className="title is-4">{this.props.name}</p>
+                <hr />
+                <p className="subtitle is-6"><a target="_blank" href={"mailto:" + this.props.email}>Email</a></p>
+                <p className="subtitle is-6"><a target="_blank" href={this.props.linked}>LinkedIn</a></p>
+                <p className="subtitle is-6"><a target="_blank" href={this.props.github}>GitHub</a></p>
+                <p className="subtitle is-6"><a target="_blank" href={this.props.portfolio}>Portfolio</a></p>
+                <p className="subtitle is-6">Primary Language: {this.props.lang}</p>
+                <p className="subtitle is-6">{
+                  (this.props.looking)
+                    ? `Desired Job: ${this.props.job}`
+                    : `Current Job: ${this.props.job}`
+                } </p>
+                <p className="subtitle is-6">{
+                  (this.props.looking)
+                    ? "Currently Seeking Employment: Yes" : "Not Currently Seeking Employment"
+                } </p>
               </div>
             </div>
           </div>
-       </div>
+        </div>
+      </div>
     );
   }
 }
